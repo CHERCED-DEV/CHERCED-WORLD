b4dcec62a06bd88886bf6b9005e4a76135aaf40f
Camilo <hitmancodeme47@hotmail.com>
2023-03-27 07:08:14 -0500

    education interfaces

--- components/Mains/aboutMeSection/utils/ListEducationPro.tsx
+++ components/Mains/aboutMeSection/utils/ListEducationPro.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { EducationConfig } from "../../../../pages/api/customCMS/interfaces";
+import { EducationDegreesConfig } from "../../../../pages/api/customCMS/interfaces";
 
-export const ListEducationPro: React.FC<EducationConfig> = (props) => {
-
+export const ListEducationPro: React.FC<EducationDegreesConfig> = (props) => {
     return (
-
         <li className="aboutMe-education__item">
             <h1 className="aboutMe-education__institution">
                 {props.proTitleDegree.proTitle}
