6452a3d2eb8f718cd7b917507ae50df34d0d49e6
Camilo <hitmancodeme47@hotmail.com>
2023-03-30 17:12:29 -0500

    enecimo intentooooo! vida mk

--- utils/providers/requests/homeCB.ts
+++ utils/providers/requests/homeCB.ts
@@ -2,11 +2,11 @@ import { CmsDataConfig } from '../../../pages/api/customCMS/interfaces'
 import { ProjectsConfig } from '../../../pages/api/projects/database/interfaces'
 
 export async function getCMSData(): Promise<CmsDataConfig> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/customCMS`);
+    const response = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/customCMS`);
     return response.json();
 } 
 
 export async function fetchProject(id: any): Promise<ProjectsConfig> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/projects/${id}`);
+    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/projects/${id}`);
     return res.json();
 }
