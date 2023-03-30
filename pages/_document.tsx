459e957f6f8d4372076def9b9790598ef7d27e0a
Camilo <hitmancodeme47@hotmail.com>
2023-03-30 15:12:30 -0500

    neverUse again getinitialprops, its drepecated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,36 +1,48 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { getCMSData } from '../utils/providers/requests/homeCB';
 import { CmsDataConfig } from './api/customCMS/interfaces';
 
-interface MyDocumentProps extends DocumentInitialProps {
-    CmsData: CmsDataConfig;
+interface MyDocumentProps {
+  CmsData: CmsDataConfig;
 }
+
 export default class MyDocument extends Document<MyDocumentProps> {
-    onload: (() => void) | null = null;
-    rel: 'preload' | 'stylesheet' = 'preload';
-    render() {
-        const { CmsData } = this.props
-        return (
-            <Html lang="en">
-                <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link rel="preload" as="font" href="/fonts/Montserrat/static/Montserrat-Regular.ttf" type="font/ttf" crossOrigin="anonymous" />
+  static async getStaticProps(ctx: DocumentContext) {
+    const CmsData = await getCMSData();
+    return {
+      props: { CmsData },
+    };
+  }
 
-                </Head>
-                <body>
-                    <Main/>
-                    <div id='modal' />
-                    <NextScript />
-                    <script dangerouslySetInnerHTML={{ __html: `localStorage.setItem('CmsData', JSON.stringify(${JSON.stringify(CmsData)}));` }} />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    const { CmsData } = this.props;
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Montserrat/static/Montserrat-Regular.ttf"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <div id="modal" />
+          <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `localStorage.setItem('CmsData', JSON.stringify(${JSON.stringify(
+                CmsData
+              )}));`,
+            }}
+          />
+        </body>
+      </Html>
+    );
+  }
 }
 
-MyDocument.getInitialProps = async (ctx: DocumentContext): Promise<MyDocumentProps & DocumentInitialProps> => {
-    const initialProps = await Document.getInitialProps(ctx);
-    const CmsData = await getCMSData();
-    return { ...initialProps, CmsData };
-};
