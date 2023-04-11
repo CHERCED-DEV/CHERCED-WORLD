899105948cc99905f60dd6649d7c2c1853530c0c
Camilo <hitmancodeme47@hotmail.com>
2023-04-10 20:37:48 -0500

    unique false

--- pages/api/contactMe/database/contactMe.model.ts
+++ pages/api/contactMe/database/contactMe.model.ts
@@ -5,24 +5,28 @@ const messageSchema = new Schema(
     name: {
       type: String,
       require: [true, "Name is required"],
+      unique: false,
       trim: true,
       maxlength: [60, "Name must be less than 60 characters"],
     },
     email: {
       type: String,
       require: [true, "Email is required"],
+      unique: false,
       trim: true,
       maxlength: [60, "Email must be less than 60 characters"],
     },
     phone: {
       type: Number,
       require: [true, "Phone is required"],
+      unique: false,
       trim: true,
       maxlength: [15, "Phone must be less than 15 characters"],
     },
     message: {
       type: String,
       require: [true, "Message is required"],
+      unique: false,
       trim: true,
       maxlength: [400, "Message must be less than 400 characters"],
     },
